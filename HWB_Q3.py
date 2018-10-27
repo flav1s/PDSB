a90bc8ad66af249ea4c2cb2739d0b0201f0fec14
Flavia Rocha <flaviaccruzr@gmail.com>
2018-10-27 20:16:09 -0300

    filter added

--- HWB_Q3.py
+++ HWB_Q3.py
@@ -1,31 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import fftpack
+import FIRFilter as ff
 
 f1 = 750
 f2 = 2500
 f3 = 3250
 
-fs = 150.0;  # taxa de amostragem
+fs = 6500.0;  # taxa de amostragem
 Ts = 1.0/fs; # intervalo de amostragem
 t = np.arange(0,1,Ts) # vetor de tempo
-s = np.sin(2 * np.pi * f1 * t) + np.sin(2 * np.pi * f2 * t) + np.sin(2 * np.pi * f3 * t)
+s = np.sin(2 * np.pi * f1 * t) + np.sin(2 * np.pi * f2 * t) + np.sin(2 * np.pi * f3 * t) # sinal
 
 # Funcao no Tempo
-plt.subplot(2, 1, 1)
+plt.figure()
+plt.title("Sinal")
 plt.plot(t,s)
 plt.grid()
 plt.xlabel('Tempo [s]')
-plt.ylabel('Amplitude');
+plt.ylabel('Amplitude')
 
-# FFT
+# fft
 X = fftpack.fft(s)
 freqs = fftpack.fftfreq(len(s)) * fs
 
 # Espectro
-plt.subplot(2, 1, 2)
+plt.title("Espectro Sinal")
 plt.stem(freqs, np.abs(X))
 plt.grid()
 plt.xlabel('Frequencia[Hz]')
 plt.ylabel('Magnitude')
-plt.show()
+plt.show(False)
+
+filter = ff.FIRFilter(500.0, 800.0, 0.1, 100.0, 20.0, fs) # low_border, high_border, ripple, transition_band, band_pass_attenuation, fs
+filter.fft(len(X))
+filter.apply_filter_frequency(X)
