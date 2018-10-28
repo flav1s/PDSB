32c6861d16844822d90de81208ec31385c76227a
Flávia Rocha <flaviaccruzr@gmail.com>
2018-10-28 20:41:38 -0300

    Update HWB_Q3.py

--- HWB_Q3.py
+++ HWB_Q3.py
@@ -7,8 +7,8 @@ f1 = 750
 f2 = 2500
 f3 = 3250
 
-fs = 6500.0;  # taxa de amostragem
-Ts = 1.0/fs; # intervalo de amostragem
+fs = 6500.0  # taxa de amostragem
+Ts = 1.0/fs # intervalo de amostragem
 t = np.arange(0,1,Ts) # vetor de tempo
 s = np.sin(2 * np.pi * f1 * t) + np.sin(2 * np.pi * f2 * t) + np.sin(2 * np.pi * f3 * t) # sinal
 
@@ -35,9 +35,6 @@ plt.grid()
 filter = ff.FIRFilter(300.0, 500.0, 0.1, 100.0, 60.0, fs) # low_border, high_border, ripple, transition_band, band_pass_attenuation, fs
 filter.fft(len(X))
 S = filter.apply_filter_frequency(X)
-
-print (len(t))
-print (len(np.abs(S)))
 
 # ifft
 plt.figure()
